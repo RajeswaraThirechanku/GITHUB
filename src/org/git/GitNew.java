3a4d8b1c334373b56423c20936e899c258dd05fd
RajeswaraThirechanku <rajeswarant2@gmail.com>
2022-05-14 17:46:39 +0530

    Rajeswara program

---
+++ src/org/git/GitNew.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class GitNew {
+	public void test1() {
+		System.out.println("1");
+	}
+
+	public void test2() {
+		System.out.println("2");
+	}
+
+	public void test3() {
+		System.out.println("3");
+
+	}
+
+	public void test4() {
+		System.out.println("4");
+	}
+}
